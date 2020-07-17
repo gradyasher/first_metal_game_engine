d26026482c8e722ea86400338cf4d2eefa80bfd8
gradyasher <gradyasher@gmail.com>
2020-07-16 23:31:05 -0500

    Initial Commit

---
+++ metullll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  metullll
+//
+//  Created by Grady Stanton on 7/16/20.
+//  Copyright © 2020 grades. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
